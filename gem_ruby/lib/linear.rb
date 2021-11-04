b5100cf8388ae17a77455ba5a5f3725372eb4654
evandro <ebolzan@inf.ufsm.br>
2021-11-04 17:11:07 -0300

    refactor: change file linear search

--- gem_ruby/lib/linear.rb
+++ gem_ruby/lib/linear.rb
@@ -1,28 +1,39 @@
 #ruby program
 #creating array
 
-def search(arr, n, x)
-    for i in 0..n
-        if arr[i] == x
-            return i
+module Linear
+
+    class LinearSearch
+
+
+        def search(arr, n, x)
+            for i in 0..n
+                if arr[i] == x
+                    return i
+                end
+            end
+
+            return -1
         end
     end
-
-    return -1
 end
 
-#creating array 
+
+=begin
+    function to search usage
+
+#creating array
 nums = Array[1, 2, 3, 4,5]
 #the element to be found
 x = 5
-#the length of array 
+#the length of array
 n = nums.length
-
-#function to search
-result = search(nums, n, x)
+obj = Linear::LinearSearch.new
+result = obj.search(nums, n, x)
 
 if result == -1
     puts "Element is not present in array"
 else
     puts "Element #{x} is present in array"
 end
+==end
