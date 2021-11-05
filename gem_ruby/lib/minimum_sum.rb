3e18cd14a8231b3014b3b2ffd0c11dfd86d8baa9
evandro <ebolzan@inf.ufsm.br>
2021-11-05 09:30:47 -0300

    refactor: change minimum sum file

--- gem_ruby/lib/minimum_sum.rb
+++ gem_ruby/lib/minimum_sum.rb
@@ -1,18 +1,19 @@
-# ruby
-# array minimization
- 
-# Function defintion for minCost
-def minSum(arr)
- 
-    # find the minimum element of A[]
-    min_val = arr.min()
- 
-    # return the answer
-    return min_val * (arr.length-1)
+module Minimum
+
+    class MinimumSum
+        # array minimization
+        # Function definition for minCost
+        def minSum(arr)
+
+            # find the minimum element of A[]
+            min_val = arr.min()
+
+            # return the answer
+            return min_val * (arr.length-1)
+        end
+    end
 end
- 
+
 # driver code
-arr =Array[7, 2, 3, 4, 5, 6]
-puts (minSum(arr))
-
-
+#arr =Array[7, 2, 3, 4, 5, 6]
+#puts (Minimum::MinimumSum.new.minSum(arr))
