64e3648b7fd7be9ef047c282926f908b2214b7a3
evandro <ebolzan@inf.ufsm.br>
2021-11-05 08:30:51 -0300

    refactor: change file buble.rb

--- gem_ruby/lib/buble.rb
+++ gem_ruby/lib/buble.rb
@@ -1,28 +1,26 @@
 #implementation for ruby
-def bubbleSort(arr)
+#sort array
+module Bubble
 
-    n = arr.length
+    class BubbleSort
 
-    for i in (1..n)
+        def bubbleSort(arr)
 
-        for j in (0..(n -i-1))
-            if arr[j] > arr[j+1]
-                arr[j], arr[j+1] = arr[j+1], arr[j]                
+            n = arr.length
+
+            for i in (1..n)
+                for j in (0..(n -i-1))
+                    if arr[j] > arr[j+1]
+                        arr[j], arr[j+1] = arr[j+1], arr[j]
+                    end
+                end
             end
         end
     end
 end
-
 #inicialize array
-arr = Array[64, 34, 25, 12, 22, 11, 90]
+#arr = Array[64, 34, 25, 12, 22, 11, 90]
 
 #run function
-bubbleSort(arr)
-
-puts "Sorted array is:"
-
-#display result impletation
-arr.each do |digit|
-    puts "In Loop #{digit}"
- end
+#Bubble::BubbleSort.new.bubbleSort(arr)
 
