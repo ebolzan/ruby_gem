eba37909eb47bce39bdd41b726f71acf333449f0
evandro <ebolzan@inf.ufsm.br>
2021-11-08 17:52:58 -0300

    refactor: change file minimum product

--- gem_ruby/lib/minumum_product.rb
+++ gem_ruby/lib/minumum_product.rb
@@ -1,91 +1,70 @@
-#minimum_product ready
-#positive then our result should be the first minimum positive number.
+module Product
+    class ProductMinimum
 
-# Ruby program to find maximum
-# product of a subset.
- 
-# def to find maximum
-# product of a subset
- 
- 
-def minProductSubset(a, n)
+        #positive then our result should be the first minimum positive number.
+        # Ruby program to find maximum product of a subset. def to find maximum product of a subset
+        def minProductSubset(a, n)
 
-    if (n == 1)
-        return a[0]
-    end
+            if (n == 1)
+                return a[0]
+            end
 
-    n-=1
- 
-    # Find count of negative numbers,
-    # count of zeros, maximum valued
-    # negative number, minimum valued
-    # positive number and product
-    # of non-zero numbers
-    max_neg = -(2**30)
-    min_pos = 2**30
-    count_neg = 0
-    count_zero = 0
-    prod = 1
+            n-=1
+            # Find count of negative numbers, count of zeros, maximum valued ,negative number, minimum valued
+            # positive number and product of non-zero numbers
+            max_neg = -(2**30)
+            min_pos = 2**30
+            count_neg = 0
+            count_zero = 0
+            prod = 1
 
-    for i in 0..n do
+            for i in 0..n do
 
-        # If number is 0, we don't
-        # multiply it with product.
-        if (a[i] == 0)
-            count_zero = count_zero + 1
-            continue
+                # If number is 0, we don't multiply it with product.
+                if (a[i] == 0)
+                    count_zero = count_zero + 1
+                    continue
+                end
+                # Count negatives and keep track of maximum valued negative.
+                if a[i].to_i < 0
+                    count_neg = count_neg + 1
+                    max_neg = [max_neg, a[i]].max
+                end
+
+                # Track minimum positive number of array
+                if a[i].to_i > 0
+                    min_pos = [min_pos, a[i]].min
+                end
+
+                prod = prod * a[i]
+            end
+
+            # If there are all zeros or no negative number present
+            if count_zero == n or (count_neg == 0 and count_zero > 0)
+                return 0
+            end
+
+            # If there are all positive
+            if (count_neg == 0)
+                return min_pos
+            end
+
+            # If there are even number of negative numbers and count_neg not 0
+            if (count_neg & 1) == 0 and
+                    count_neg != 0
+
+                # Otherwise result is product of all non-zeros divided by maximum valued negative.
+                prod = int(prod / max_neg)
+            end
+
+            return prod
+
         end
- 
-        # Count negatives and keep
-        # track of maximum valued
-        # negative.
-        if a[i].to_i < 0
-            count_neg = count_neg + 1
-            max_neg = [max_neg, a[i]].max
-        end
- 
-        # Track minimum positive
-        # number of array
-        if a[i].to_i > 0
-            min_pos = [min_pos, a[i]].min
-        end
 
-        prod = prod * a[i]
     end
- 
-    # If there are all zeros
-    # or no negative number
-    # present
-    if count_zero == n or (count_neg == 0 and count_zero > 0)
-        return 0
-    end
- 
-    # If there are all positive
-    if (count_neg == 0)
-        return min_pos
-    end
- 
-    # If there are even number of
-    # negative numbers and count_neg
-    # not 0
-    if (count_neg & 1) == 0 and
-            count_neg != 0
- 
-        # Otherwise result is product of
-        # all non-zeros divided by
-        # maximum valued negative.
-
-        prod = int(prod / max_neg)
-    end
- 
-    return prod
-
 end
- 
- 
 # Driver code
 a = Array[-1, -1, -2, 4, 3]
 n = a.length
-puts(minProductSubset(a, n))
-# This code is contributed by
-# Manish Shaw (manishshaw1)
+result = Product::ProductMinimum.new.minProductSubset(a, n)
+puts(result)
